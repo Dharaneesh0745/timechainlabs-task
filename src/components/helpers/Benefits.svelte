7859e7dcc7e346c4d4d6e3098affe9c4f669b700
Dharaneesh0745 <dharaneesh0745@gmail.com>
2024-09-27 03:28:26 +0530

    Developed the benefits section.

--- src/components/helpers/Benefits.svelte
+++ src/components/helpers/Benefits.svelte
@@ -1,6 +1,42 @@
 <script></script>
 
-<main class="mt-16 flex gap-x-32">
-  <div class="flex-1 bg-gray-400">image</div>
-  <div class="flex-1 bg-gray-400">content</div>
+<main class="mt-24 flex lg:flex-row gap-x-32 flex-col">
+  <div class="flex-1">
+    <img src="/benefits.jpg" class="rounded-xl lg:h-[480px]" alt="" />
+  </div>
+  <div class="flex-1 lg:mt-0 mt-10">
+    <h1 class="text-blue-600 font-medium text-xl">BENEFITS</h1>
+    <br />
+    <h1 class="text-4xl">Why hire from us?</h1>
+    <br />
+    <p class="text-gray-400">
+      Together, we're making education accessible and transformative. <br />Join
+      us in shaping a brighter future!
+    </p>
+    <br />
+    <ul class="flex flex-col gap-y-8">
+      <li>
+        <i class="fa-solid fa-circle-check text-green-500 text-xl"></i>
+        <span class="text-xl">&nbsp;Trained to meed Industry Standard</span>
+      </li>
+      <li>
+        <i class="fa-solid fa-circle-check text-green-500 text-xl"></i>
+        <span class="text-xl">&nbsp;Independently pre-assessed</span>
+      </li>
+      <li>
+        <i class="fa-solid fa-circle-check text-green-500 text-xl"></i>
+        <span class="text-xl">&nbsp;Immediate Availability</span>
+      </li>
+      <li>
+        <i class="fa-solid fa-circle-check text-green-500 text-xl"></i>
+        <span class="text-xl">&nbsp;Hassle Free Experience</span>
+      </li>
+      <li>
+        <button
+          class="text-white lg:text-base hover:bg-blue-500 hover:duration-200 bg-blue-600 px-5 py-3 rounded-md text-sm"
+          >Request Talent Today
+        </button>
+      </li>
+    </ul>
+  </div>
 </main>
